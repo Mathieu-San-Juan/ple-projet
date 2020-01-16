01316d6dafc036c3760ad88db30385e906a55f84
ArthurPeyrichou <arthurpeyrichou@yahoo.fr>
2020-01-16 12:42:55 +0100

    Exo 6 revut au gout du prof

--- EXO6/src/main/java/bigdata/Exo6.java
+++ EXO6/src/main/java/bigdata/Exo6.java
@@ -49,11 +49,27 @@ public class Exo6 {
 			} ).map(v -> 
 				{ 
 				String[] split = v.split(";");
-				return split[2] + ";" + split[3];
+				return split[2] + ";" + split[3] + ";" + split[4];
 
 				});
-		//on sépare les patterns
-		JavaPairRDD<String, Double> totalDurationPerPattern = notIdle.flatMapToPair(line -> 
+		StorageLevel sl = new StorageLevel();
+		notIdle = notIdle.persist(sl);
+		
+		//D'abords les patterns qui arrivent seul
+		JavaPairRDD<String, Double> totalDurationPerPatternSingle = notIdle.filter(activity -> 
+			{
+				return activity.split(";")[2].equals("1");
+			} 
+		).mapToPair(activity -> 
+			{
+				String[] split = activity.split(";");
+				return new Tuple2<String,Double>(split[1], Double.parseDouble(split[0]));
+			} 
+		).reduceByKey((c1, c2) -> c1 + c2);
+		totalDurationPerPatternSingle = totalDurationPerPatternSingle.persist(sl);
+		
+		//Puis les patterns qui arrivent en groupe, on sépare les patterns
+		JavaPairRDD<String, Double> totalDurationPerPatternGrouped = notIdle.flatMapToPair(line -> 
 			{
 				String[] split = line.split(";");
 				String[] patterns = split[1].split(",");
@@ -65,10 +81,73 @@ public class Exo6 {
 				 return al.iterator();
 			 }
 		).reduceByKey((c1, c2) -> c1 + c2);
-		StorageLevel sl = new StorageLevel();
-		totalDurationPerPattern = totalDurationPerPattern.persist(sl);
+		notIdle.unpersist();
+		totalDurationPerPatternGrouped = totalDurationPerPatternGrouped.persist(sl);
 		
 		
+		String distriSinglepattern = distribution(totalDurationPerPatternSingle);
+		String toptenSinglepattern = topTen(totalDurationPerPatternSingle, topN);
+		totalDurationPerPatternSingle.unpersist();
+		
+		String distriGroupedpattern = distribution(totalDurationPerPatternGrouped);
+		String toptenGroupedpattern = topTen(totalDurationPerPatternGrouped, topN);
+		totalDurationPerPatternGrouped.unpersist();
+		
+		
+		// EXERCICE A : Les 22 patterns avec leurs temps total de duree en pourcentage
+		//durationPercentByPattern.saveAsTextFile("EXO6/durationPercent/");
+		try {
+			writeToLocal(context, "Exo6/singlePatternPercentDuree.txt", distriSinglepattern);
+		} catch(IOException ex){
+			System.err.println("############################################");
+			System.err.println("Problème avec l'écriture sur fichier en HDFS");
+			System.err.println("############################################");
+		}
+		try {
+			writeToLocal(context, "Exo6/groupedPatternPercentDuree.txt", distriGroupedpattern);
+		} catch(IOException ex){
+			System.err.println("############################################");
+			System.err.println("Problème avec l'écriture sur fichier en HDFS");
+			System.err.println("############################################");
+		}
+
+		// EXERCICE B : Top 10 pattern en représentativité.
+		try {
+			writeToLocal(context, "Exo6/topTenSinglePatternPercentDuree.txt", toptenSinglepattern);
+		} catch(IOException ex){
+			System.err.println("############################################");
+			System.err.println("Problème avec l'écriture sur fichier en HDFS");
+			System.err.println("############################################");
+		}
+		try {
+			writeToLocal(context, "Exo6/topTenGroupedPatternPercentDuree.txt", toptenGroupedpattern);
+		} catch(IOException ex){
+			System.err.println("############################################");
+			System.err.println("Problème avec l'écriture sur fichier en HDFS");
+			System.err.println("############################################");
+		}
+
+		
+		//Partie affichage des exo A et B
+		System.out.println("######  EXO 6 : A ######");
+		System.out.println("Pourcentage du temps total de phases où chaque motif a été observé seul");
+		System.out.println(distriSinglepattern);
+		System.out.println("Pourcentage du temps total de phases où chaque motif a été observé concurrent");
+		System.out.println(distriGroupedpattern);
+		System.out.println("######  EXO 6 : B ######");
+		System.out.println("Top 10 pattern seul : ");
+		System.out.println(toptenSinglepattern);
+		System.out.println("Top 10 pattern concurrent : ");
+		System.out.println(toptenGroupedpattern);
+
+		context.close();
+	}
+	
+	/*
+     * @brief Permet de recuperer les total des durer en pourcentage pour les 22 motifs
+	 * @param totalDurationPerPattern contenant en key l'id du pattern et en value le total
+	*/
+	public static String distribution(JavaPairRDD<String, Double> totalDurationPerPattern) {
 		JavaDoubleRDD aPatternDuration = totalDurationPerPattern.mapToDouble(tuple -> 
 		{
 			return tuple._2;
@@ -80,38 +159,17 @@ public class Exo6 {
 		JavaPairRDD<String, Double> durationPercentByPattern = totalDurationPerPattern.mapToPair(tuple -> {
 			return new Tuple2<String,Double>(tuple._1, (Double)(tuple._2 * 100.0D / totalSumOfDuration ));
 		} );
-		
-		//Que 22 motifs donc collect pas méchant mais sinon on ecrirait directement avec un saveastextfile
-		List<Tuple2<String,Double>> durationPercentByPatternList = durationPercentByPattern.collect();
-		//durationPercentByPattern.saveAsTextFile("EXO6/durationPercent/");
-		try {
-			writeToLocal(context, "Exo6/patternPercentDuree.txt", durationPercentByPatternList.toString());
-		} catch(IOException ex){
-			System.err.println("############################################");
-			System.err.println("Problème avec l'écriture sur fichier en HDFS");
-			System.err.println("############################################");
-		}
-
-		// EXERCICE B : Top 10 pattern en représentativité.
-		List<Tuple2<String,Double>> topNPatternPercentDuration = durationPercentByPattern.top(topN,SerializableComparator.serialize((v, w) -> {return Double.compare(v._2, w._2); } ) );
-		try {
-			writeToLocal(context, "Exo6/topTenPatternPercentDuree.txt", topNPatternPercentDuration.toString());
-		} catch(IOException ex){
-			System.err.println("############################################");
-			System.err.println("Problème avec l'écriture sur fichier en HDFS");
-			System.err.println("############################################");
-		}
-
-		
-		//Partie affichage des exo A et B
-		System.out.println("######  EXO 6 : A ######");
-		System.out.println("Pourcentage du temps total de phases où chaque motif a été observé seul ou concurrent");
-		System.out.println(durationPercentByPatternList.toString());
-		System.out.println("######  EXO 6 : B ######");
-		System.out.println("Top 10 pattern : ");
-		System.out.println(topNPatternPercentDuration.toString());
-
-		context.close();
+		return durationPercentByPattern.collect().toString();
+	}
+	
+	/*
+     * @brief Permet de créer et d'ecrire dans un fichier en HDFS
+	 * @param totalDurationPerPattern contenant en key l'id du pattern et en value le total
+	 * @param topN le top que l'on souhaite
+	*/
+	public static String topTen(JavaPairRDD<String, Double> totalDurationPerPattern, int topN) {
+		List<Tuple2<String,Double>> topNPatternPercentDuration = totalDurationPerPattern.top(topN,SerializableComparator.serialize((v, w) -> {return Double.compare(v._2, w._2); } ) );
+		return topNPatternPercentDuration.toString();
 	}
 	
 	public interface SerializableComparator<T> extends Comparator<T>, Serializable {
